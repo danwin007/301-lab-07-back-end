b0436e2ec866374aecc0104232a93fce88ff4470
dannevernguyens <danguyenuw@gmail.com>
2020-01-22 19:59:23 -0800

    figured out the .map, we forgot to return the new weather object. now it works with .map on the json

--- server.js
+++ server.js
@@ -41,16 +41,31 @@ app.get('/location', (request, response) => {
 function Weather (skyData) {
   this.forecast = skyData.summary;
   this.time = new Date(skyData.time * 1000).toDateString();
-  allWeather.push(this);
+  // allWeather.push(this);
 }
 
-let allWeather=[];
+// let allWeather=[];
+
+app.get('/weather', (request, response) => {
+  try{
+    const skyData = require('./data/darksky.json');
+    let allWeather = skyData.daily.data.map((byDate) => {
+      return new Weather(byDate);
+    });
+    console.log(allWeather);
+    response.send(allWeather);
+  }
+  catch(error){
+    errorHandler('Not today, satan.', request, response);
+  }
+})
 
 // app.get('/weather', (request, response) => {
 //   try{
 //     const skyData = require('./data/darksky.json');
-//     let allWeather = skyData.daily.data.map ((byDate) => {
+//     skyData.daily.data.forEach ((byDate) => {
 //       new Weather(byDate);
+//       console.log(allWeather);
 //     });
 //     response.send(allWeather);
 //   }
@@ -58,20 +73,6 @@ let allWeather=[];
 //     errorHandler('Not today, satan.', request, response);
 //   }
 // })
-
-app.get('/weather', (request, response) => {
-  try{
-    const skyData = require('./data/darksky.json');
-    skyData.daily.data.forEach ((byDate) => {
-      new Weather(byDate);
-      console.log(allWeather);
-    });
-    response.send(allWeather);
-  }
-  catch(error){
-    errorHandler('Not today, satan.', request, response);
-  }
-})
 
 
 
